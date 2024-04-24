b81376a0da67538bd3189ebfcf66a29e6d4ea991
Bruno Afonso <brunoafonso2003@gmail.com>
2024-04-24 21:18:40 +0100

    teste

--- Projeto/base_conhecimento.pl
+++ Projeto/base_conhecimento.pl
@@ -65,70 +65,70 @@ if rc_normal and temp_normal and ig_nao and tt_sangue and d_nao then 62.
 if rc_normal and temp_normal and ig_nao and tt_nulo and d_sim then 63.
 if rc_normal and temp_normal and ig_nao and tt_nulo and d_nao then 64.
 
-perfil(1):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_seca, d_sim) .
-perfil(2):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_seca, d_nao) .
-perfil(3):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_espeturacao, d_sim) .
-perfil(4):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_espeturacao, d_nao) .
-perfil(5):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_sangue, d_sim) .
-perfil(6):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_sangue, d_nao) .
-perfil(7):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_nulo, d_sim) .
-perfil(8):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_nulo, d_nao) .
-perfil(9):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_seca, d_sim) .
-perfil(10):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_seca, d_nao) .
-perfil(11):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_espeturacao, d_sim) .
-perfil(12):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_espeturacao, d_nao) .
-perfil(13):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_sangue, d_sim) .
-perfil(14):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_sangue, d_nao) .
-perfil(15):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_nulo, d_sim) .
-perfil(16):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_nulo, d_nao) .
-perfil(17):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_seca, d_sim) .
-perfil(18):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_seca, d_nao) .
-perfil(19):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_espeturacao, d_sim) .
-perfil(20):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_espeturacao, d_nao) .
-perfil(21):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_sangue, d_sim) .
-perfil(22):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_sangue, d_nao) .
-perfil(23):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_nulo, d_sim) .
-perfil(24):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_nulo, d_nao) .
-perfil(25):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_seca, d_sim) .
-perfil(26):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_seca, d_nao) .
-perfil(27):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_espeturacao, d_sim) .
-perfil(28):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_espeturacao, d_nao) .
-perfil(29):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_sangue, d_sim) .
-perfil(30):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_sangue, d_nao) .
-perfil(31):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_nulo, d_sim) .
-perfil(32):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_nulo, d_nao) .
-perfil(33):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_seca, d_sim) .
-perfil(34):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_seca, d_nao) .
-perfil(35):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_espeturacao, d_sim) .
-perfil(36):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_espeturacao, d_nao) .
-perfil(37):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_sangue, d_sim) .
-perfil(38):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_sangue, d_nao) .
-perfil(39):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_nulo, d_sim) .
-perfil(40):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_nulo, d_nao) .
-perfil(41):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_seca, d_sim) .
-perfil(42):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_seca, d_nao) .
-perfil(43):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_espeturacao, d_sim) .
-perfil(44):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_espeturacao, d_nao) .
-perfil(45):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_sangue, d_sim) .
-perfil(46):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_sangue, d_nao) .
-perfil(47):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_nulo, d_sim) .
-perfil(48):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_nulo, d_nao) .
-perfil(49):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_seca, d_sim) .
-perfil(50):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_seca, d_nao) .
-perfil(51):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_espeturacao, d_sim) .
-perfil(52):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_espeturacao, d_nao) .
-perfil(53):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_sangue, d_sim) .
-perfil(54):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_sangue, d_nao) .
-perfil(55):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_nulo, d_sim) .
-perfil(56):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_nulo, d_nao) .
-perfil(57):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_seca, d_sim) .
-perfil(58):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_seca, d_nao) .
-perfil(59):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_espeturacao, d_sim) .
-perfil(60):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_espeturacao, d_nao) .
-perfil(61):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_sangue, d_sim) .
-perfil(62):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_sangue, d_nao) .
-perfil(63):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_nulo, d_sim) .
-perfil(64):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_nulo, d_nao) .
+perfil(1, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_seca, d_sim, Z).
+perfil(2, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_seca, d_nao, Z).
+perfil(3, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_espeturacao, d_sim, Z).
+perfil(4, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_espeturacao, d_nao, Z).
+perfil(5, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_sangue, d_sim, Z).
+perfil(6, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_sangue, d_nao, Z).
+perfil(7, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_nulo, d_sim, Z).
+perfil(8, Z):- verdiagnostico(rc_alt, temp_alta, ig_sim, tt_nulo, d_nao, Z).
+perfil(9, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_seca, d_sim, Z).
+perfil(10, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_seca, d_nao, Z).
+perfil(11, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_espeturacao, d_sim, Z).
+perfil(12, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_espeturacao, d_nao, Z).
+perfil(13, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_sangue, d_sim, Z).
+perfil(14, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_sangue, d_nao, Z).
+perfil(15, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_nulo, d_sim, Z).
+perfil(16, Z):- verdiagnostico(rc_alt, temp_alta, ig_nao, tt_nulo, d_nao, Z).
+perfil(17, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_seca, d_sim, Z).
+perfil(18, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_seca, d_nao, Z).
+perfil(19, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_espeturacao, d_sim, Z).
+perfil(20, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_espeturacao, d_nao, Z).
+perfil(21, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_sangue, d_sim, Z).
+perfil(22, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_sangue, d_nao, Z).
+perfil(23, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_nulo, d_sim, Z).
+perfil(24, Z):- verdiagnostico(rc_alt, temp_normal, ig_sim, tt_nulo, d_nao, Z).
+perfil(25, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_seca, d_sim, Z).
+perfil(26, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_seca, d_nao, Z).
+perfil(27, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_espeturacao, d_sim, Z).
+perfil(28, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_espeturacao, d_nao, Z).
+perfil(29, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_sangue, d_sim, Z).
+perfil(30, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_sangue, d_nao, Z).
+perfil(31, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_nulo, d_sim, Z).
+perfil(32, Z):- verdiagnostico(rc_alt, temp_normal, ig_nao, tt_nulo, d_nao, Z).
+perfil(33, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_seca, d_sim, Z).
+perfil(34, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_seca, d_nao, Z).
+perfil(35, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_espeturacao, d_sim, Z).
+perfil(36, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_espeturacao, d_nao, Z).
+perfil(37, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_sangue, d_sim, Z).
+perfil(38, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_sangue, d_nao, Z).
+perfil(39, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_nulo, d_sim, Z).
+perfil(40, Z):- verdiagnostico(rc_normal, temp_alta, ig_sim, tt_nulo, d_nao, Z).
+perfil(41, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_seca, d_sim, Z).
+perfil(42, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_seca, d_nao, Z).
+perfil(43, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_espeturacao, d_sim, Z).
+perfil(44, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_espeturacao, d_nao, Z).
+perfil(45, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_sangue, d_sim, Z).
+perfil(46, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_sangue, d_nao, Z).
+perfil(47, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_nulo, d_sim, Z).
+perfil(48, Z):- verdiagnostico(rc_normal, temp_alta, ig_nao, tt_nulo, d_nao, Z).
+perfil(49, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_seca, d_sim, Z).
+perfil(50, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_seca, d_nao, Z).
+perfil(51, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_espeturacao, d_sim, Z).
+perfil(52, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_espeturacao, d_nao, Z).
+perfil(53, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_sangue, d_sim, Z).
+perfil(54, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_sangue, d_nao, Z).
+perfil(55, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_nulo, d_sim, Z).
+perfil(56, Z):- verdiagnostico(rc_normal, temp_normal, ig_sim, tt_nulo, d_nao, Z).
+perfil(57, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_seca, d_sim, Z).
+perfil(58, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_seca, d_nao, Z).
+perfil(59, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_espeturacao, d_sim, Z).
+perfil(60, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_espeturacao, d_nao, Z).
+perfil(61, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_sangue, d_sim, Z).
+perfil(62, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_sangue, d_nao, Z).
+perfil(63, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_nulo, d_sim, Z).
+perfil(64, Z):- verdiagnostico(rc_normal, temp_normal, ig_nao, tt_nulo, d_nao, Z).
 
 
 
