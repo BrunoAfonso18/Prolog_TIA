5888528fba8cf4c059841142d7ffb772724a972e
Bruno Afonso <brunoafonso2003@gmail.com>
2024-04-30 22:14:20 +0100

    initial 1B

---
+++ Projeto/P1B_exemplo/restaurantes_ifthen.pl
@@ -0,0 +1,4 @@
+:- dynamic (<==)/2.
+
+nao<==[[idade=idade36a45, aspeto=atendimento], [idade=idade46a55], [tipo=hamburguers], [aspeto=variedade, idade=idade18a25]].
+sim<==[[aspeto=comida], [aspeto=atendimento], [preco=precoacima20], [idade=idade15a17], [tipo=francesinha], [preco=preco5a7, tipo=carne]].
---
+++ Projeto/P1B_exemplo/satisfy.pl
@@ -0,0 +1,6 @@
+# code by Ivan Bratko, 2011:
+
+satisfy( Object, Conj)  :-
+   \+ ( member( Att = Val, Conj),
+         member( Att = ValX, Object),
+         ValX \== Val).
