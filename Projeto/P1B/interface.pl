3f43e46d0653ea679ee16b7c79b83434c9156d06
Bruno Afonso <brunoafonso2003@gmail.com>
2024-05-01 20:58:18 +0100

    interface parte B

--- Projeto/P1B/interface.pl
+++ Projeto/P1B/interface.pl
@@ -1,22 +1,20 @@
-% Paulo Cortez 2021@
-% Beatriz Silva, Daniela Ferreira, Márcia Veloso, Diogo Silva (2021)
-:-[satisfy, induce_ifthen, restaurantes_ifthen].
+:-[satisfy, induce_ifthen, doencas_ifthen].
 
 % the goal is to classify 
 learn_rules:-
 	learn(nao),  % negative examples
 	learn(sim), % positive examples
         % save new rules:
-	tell('restaurantes_ifthen.pl'),
+	tell('doencas_ifthen.pl'),
 	listing(<==),
 	told.
 
 % example of classifying
-q1(Class):- classify([idade=idade18a25,aspeto=preco,tipo=carne,preco=preco5a7],Class), write(classify([idade=idade18a25,aspeto=preco,tipo=carne,preco=preco5a7],Class)).
-q2(Class):- classify([idade=idade18a25,aspeto=variedade,tipo=carne,preco=preco7a10],Class).
+q1(Class):- classify([idade=idade18a25,Area=preco,tipo=carne,preco=preco5a7],Class), write(classify([idade=idade18a25,Area=preco,tipo=carne,preco=preco5a7],Class)).
+q2(Class):- classify([idade=idade18a25,Area=variedade,tipo=carne,preco=preco7a10],Class).
 q3(Class):- classify([tipo=francesinha, preco=preco5a7],Class).
-q7(Class):- classify([tipo=hamburguer, aspeto=variedade, preco=preco5a7],Class).
-q5(Class):- classify([idade=idademaior55,aspeto=comida,tipo=peixe,preco=preco15a20], Class).
+q7(Class):- classify([tipo=hamburguer, Area=variedade, preco=preco5a7],Class).
+q5(Class):- classify([idade=idademaior55,Area=comida,tipo=peixe,preco=preco15a20], Class).
 
 q4(Class):- findall(A,fact(A),Z), classify(Z,Class), retractall(fact(_)).
 
@@ -25,15 +23,17 @@ q4(Class):- findall(A,fact(A),Z), classify(Z,Class), retractall(fact(_)).
 menu :- 
      write(
 "
-                  Tem duvidas entre comprar uma refeicao num restaurante ou num hipermercado?
-                  Seja bem-vindo ao sistema de recomendacao! Esperamos corresponder as suas expetativas *_*
+                  Tem duvidas entre ir ou não ao hospital mais proximo de si?
+                  Tem duvidas entre tomar ou não medicação ?
+                  Este sistema consiste num processo de recomendacao na categoria de doencas pulmonares
+                  Seja bem-vindo!
 
 "),nl,
 
     write(
 "____________________________________________________________________________________________________
  
-                    Indique a sua idade
+                    Indique a sua idade?
 
                     1- 15 a 17
                     2- 18 a 25
@@ -59,75 +59,127 @@ ________________________________________________________________________________
     write(
 "____________________________________________________________________________________________________
  
-                    Qual o principal aspeto que tem em consideracao quando vai consumir uma refeicao?
+                    Qual a sua area de residencia?
 
-                    1- Qualidade da comida
-                    2- Preco
-                    3- Qualidade do atendimento
-                    4- Variedade da comida
-                    5- Tempo de espera/entrega
- 
+                    1- Cidade
+                    2- Campo
+
                     0- Sair 
 
 
 ____________________________________________________________________________________________________"),nl,nl,
-    read(Aspeto),
-        ((Aspeto == 0), halt;
-        (Aspeto == 1), assert(fact(aspeto=comida));
-        (Aspeto == 2), assert(fact(aspeto=preco));
-        (Aspeto == 3), assert(fact(aspeto=atendimento));
-        (Aspeto == 4), assert(fact(aspeto=variedade));
-        (Aspeto == 5), assert(fact(aspeto=tempoesperaentrega))), nl, 
+    read(Area),
+        ((Area == 0), halt;
+        (Area == 1), assert(fact(Area=cidade));
+        (Area == 2), assert(fact(Area=campo))), nl, 
 
     write(
 "____________________________________________________________________________________________________
- 
-                    Qual o tipo de comida que pretende consumir ?
-
-                    1- Opcoes Vegetarianas
-                    2- Opcoes Vegan
-                    3- Hamburgers
-                    4- Pizza
-                    5- Francesinha e similares
-                    6- Pratos de carne
-                    7- Pratos de peixe
- 
+             
+                    Está gravida?
+            
+                    1- Sim
+                    2- Nao
+            
                     0- Sair 
-
-
+            
+            
 ____________________________________________________________________________________________________"),nl,nl,
-    read(Tipoprato),
-        ((Tipoprato == 0), halt;
-        (Tipoprato == 1), assert(fact(tipo=vegetarianas));
-        (Tipoprato == 2), assert(fact(tipo=vegan));
-        (Tipoprato == 3), assert(fact(tipo=hamburguers));
-        (Tipoprato == 4), assert(fact(tipo=pizza));
-        (Tipoprato == 5), assert(fact(tipo=francesinha));
-        (Tipoprato == 6), assert(fact(tipo=carne));
-        (Tipoprato == 7), assert(fact(tipo=peixe))), nl, 
+    read(Gravidez),
+        ((Gravidez == 0), halt;
+        (Gravidez == 1), assert(fact(Gravidez=gravida_sim));
+        (Gravidez == 2), assert(fact(Gravidez=gravida_nao))), nl, 
 
-    write(
+write(
 "____________________________________________________________________________________________________
-
-                    Qual o preco que pretende gastar ?
-
-                    1- 5 a 7 euros
-                    2- 7 a 10 euros
-                    3- 10 a 15 euros
-                    4- 15 a 20 euros
-                    5- Acima de 20 euros
- 
+             
+                    Tem febre?
+            
+                    1- Sim
+                    2- Nao
+            
                     0- Sair 
-
-
+            
+            
 ____________________________________________________________________________________________________"),nl,nl,
-    read(Preco),
-        ((Preco == 0), halt;
-        (Preco == 1), assert(fact(preco=preco5a7));
-        (Preco == 2), assert(fact(preco=preco7a10));
-        (Preco == 3), assert(fact(preco=preco10a15));
-        (Preco == 4), assert(fact(preco=preco15a20));
-        (Preco == 5), assert(fact(preco=precoacima20))), nl,
+    read(Temp),
+        ((Temp == 0), halt;
+        (Temp == 1), assert(fact(Temp=temp_alta));
+        (Temp == 2), assert(fact(Temp=temp_normal))), nl,
+
+write(
+"____________________________________________________________________________________________________
+             
+                    Tem o ritmo cardiaco elevado?
+            
+                    1- Sim
+                    2- Nao
+            
+                    0- Sair 
+            
+            
+____________________________________________________________________________________________________"),nl,nl,
+    read(Rc),
+        ((Rc == 0), halt;
+        (Rc == 1), assert(fact(Rc=rc_alt));
+        (Rc == 2), assert(fact(Rc=rc_normal))), nl,
+
+write(
+"____________________________________________________________________________________________________
+             
+                    Tem inchaco ou incomodo na garganta?
+            
+                    1- Sim
+                    2- Nao
+            
+                    0- Sair 
+            
+            
+____________________________________________________________________________________________________"),nl,nl,
+    read(Ig),
+        ((Ig == 0), halt;
+        (Ig == 1), assert(fact(Ig=ig_sim));
+        (Ig == 2), assert(fact(Ig=ig_nao))), nl,
+
+write(
+"____________________________________________________________________________________________________
+                         
+                    Tem algum tipo de tosse?
+                        
+                    1- Tosse Seca
+                    2- Tosse com Espeturacao
+                    3- Tosse com Sangue
+                    4- Sem Tosse
+
+                    0- Sair 
+                        
+                        
+____________________________________________________________________________________________________"),nl,nl,
+    read(Tosse),
+        ((Tosse == 0), halt;
+        (Tosse == 1), assert(fact(Tosse=tt_seca));
+        (Tosse == 2), assert(fact(Tosse=tt_espeturacao));
+        (Tosse == 3), assert(fact(Tosse=tt_sangue));
+        (Tosse == 4), assert(fact(Tosse=tt_nulo))), nl,
+
+
+write(
+"____________________________________________________________________________________________________
+                         
+                    Apresenta dispneia ou dificuldade em respirar?
+                        
+                    1- Sim
+                    2- Nao
+                        
+                    0- Sair 
+                        
+                        
+____________________________________________________________________________________________________"),nl,nl,
+    read(Disp),
+        ((Disp == 0), halt;
+        (Disp == 1), assert(fact(Disp=d_sim));
+        (Disp == 2), assert(fact(Disp=d_nao))), nl,
+
 
 write("____________________________________________________________________________________________________"),nl,nl,
 
@@ -135,8 +187,9 @@ write("_________________________________________________________________________
 
 q4(C),
 
- write('Refeicao em Restaurante: '), write(C),nl,nl,
+ write('Ir ao hospital: '), write(C),nl,nl,
+ write('Tomar medicacao: '), write(C),nl,nl,
 
  
 
- write("Disfrute da sua refeicao! Obrigada!"),nl.
+ write("Obrigado pela participacao!"),nl.
