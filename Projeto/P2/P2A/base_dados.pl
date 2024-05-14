5f917bc71d32542f2ddd7434c8666d3c28d74e70
Bruno Afonso <brunoafonso2003@gmail.com>
2024-05-14 16:49:30 +0100

    custos

--- Projeto/P2/P2A/base_dados.pl
+++ Projeto/P2/P2A/base_dados.pl
@@ -1,28 +1,43 @@
-%passageiros(Paragem, NumeroPassageiros)
-passageiros(braga, 5).
-passageiros(viana_castelo, 10).
-passageiros(esposende, 3).
-passageiros(povoa_varzim, 7).
-passageiros(guimaraes, 24).
-passageiros(penafiel, 15).
-passageiros(aveiro, 40).
-passageiros(porto, 0).
+%custos(Tratamento, Custo)
+custos(paracetamol, 5).
+custos(brufen, 4).
+custos(aspirina, 2).
+custos(anti_histaminico, 8).
+custos(descanso, 100).
+custos(fisioterapia, 150).
+custos(cine_terapia, 200).
+custos(desporto, 0).
+custos(cirurgia, 1200).
+custos(inalacoes, 50).
+custos(insulina, 34).
+custos(antibiotico, 25).
+custos(dieta, 0).
+custos(ginastica, 90).
+custos(internamento, 250).
+custos(dialise, 100).
+custos(calmante, 80).
+custos(injecoes, 75).
+custos(inalo_terapia, 90).
+custos(soro, 15).
+custos(oculos, 500).
+custos(lentes_contacto, 300).
+custos(ambulatorio, 800).
 
 
-%percurso(Origem, Destino, DistanciaEmKm)
-percurso(braga, viana_castelo, 55).
-percurso(braga, esposende, 34).
-percurso(braga, povoa_varzim, 55).
-percurso(braga, aveiro, 100).
-percurso(braga, guimaraes, 25).
-percurso(viana_castelo, esposende, 25).
-percurso(viana_castelo, guimaraes, 75).
-percurso(esposende, povoa_varzim, 15).
-percurso(esposende, porto, 50).
-percurso(guimaraes, penafiel, 40).
-percurso(povoa_varzim, porto, 30).
-percurso(penafiel, porto, 20).
-percurso(aveiro, porto, 25).
+%tempo(Origem, Destino, Tempo)
+tempo(inicio, anti_histaminico, 5).
+tempo(anti_histaminico, antibiotico, 8).
+tempo(braga, povoa_varzim, 55).
+tempo(braga, aveiro, 100).
+tempo(braga, guimaraes, 25).
+tempo(viana_castelo, esposende, 25).
+tempo(viana_castelo, guimaraes, 75).
+tempo(esposende, povoa_varzim, 15).
+tempo(esposende, porto, 50).
+tempo(guimaraes, penafiel, 40).
+tempo(povoa_varzim, porto, 30).
+tempo(penafiel, porto, 20).
+tempo(aveiro, porto, 25).
 
 %arco(Origem, Destino)
 arco(braga, viana_castelo).
